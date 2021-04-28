8c0d79f26674e86009f3accda53fe00c3274d023
LUHAN BRITO BORGES <luhan.bborges@senacsp.edu.br>
2021-04-28 15:03:16 -0300

    Adicionar arquivos de projeto.

---
+++ ComercialSys/Classes/Banco.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComercialSys.Classes
+{
+    class Banco
+    {
+    }
+}
---
+++ ComercialSys/Classes/Pedido.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComercialSys.Classes
+{
+    class Pedido
+    {
+        public int Id { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+        public Usuario Usuario { get; set; }
+
+        public double Desconto { get; set; }
+
+        public string Situacao { get; set; }
+
+        public List<Item> Items { get; set; }
+    }
+}
